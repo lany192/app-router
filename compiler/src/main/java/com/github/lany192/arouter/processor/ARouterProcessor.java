c3b202fc8f21823253577e147aff4aa19d21d608
lany192 <lany192@qq.com>
2021-03-23 23:04:15 +0800

    update

--- compiler/src/main/java/com/github/lany192/arouter/processor/ARouterProcessor.java
+++ compiler/src/main/java/com/github/lany192/arouter/processor/ARouterProcessor.java
@@ -15,10 +15,7 @@ import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
-import com.squareup.javapoet.ParameterSpec;
-import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
-import com.squareup.javapoet.WildcardTypeName;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.MapUtils;
@@ -31,7 +28,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.TreeSet;
 
 import javax.annotation.processing.Filer;
@@ -53,23 +49,13 @@ import static com.github.lany192.arouter.utils.Consts.ACTIVITY;
 import static com.github.lany192.arouter.utils.Consts.ANNOTATION_TYPE_AUTOWIRED;
 import static com.github.lany192.arouter.utils.Consts.ANNOTATION_TYPE_ROUTE;
 import static com.github.lany192.arouter.utils.Consts.FRAGMENT;
-import static com.github.lany192.arouter.utils.Consts.IPROVIDER_GROUP;
-import static com.github.lany192.arouter.utils.Consts.IROUTE_GROUP;
-import static com.github.lany192.arouter.utils.Consts.METHOD_LOAD_INTO;
-import static com.github.lany192.arouter.utils.Consts.NAME_OF_GROUP;
-import static com.github.lany192.arouter.utils.Consts.PACKAGE_OF_GENERATE_FILE;
 import static com.github.lany192.arouter.utils.Consts.SERVICE;
-import static com.github.lany192.arouter.utils.Consts.WARNING_TIPS;
-import static javax.lang.model.element.Modifier.PUBLIC;
 
 @AutoService(Processor.class)
 @SupportedAnnotationTypes({ANNOTATION_TYPE_ROUTE, ANNOTATION_TYPE_AUTOWIRED})
 public class ARouterProcessor extends BaseProcessor {
     private Filer filer;
-
     private Map<String, Set<RouteMeta>> groupMap = new HashMap<>(); // ModuleName and routeMeta.
-    private Map<String, String> rootMap = new TreeMap<>();  // Map of root metas, used for generate class file in order.
-
     private TypeMirror iProvider = null;
 
     @Override
@@ -111,7 +97,7 @@ public class ARouterProcessor extends BaseProcessor {
                 MethodSpec methodSpec = MethodSpec.methodBuilder(Utils.toLowerCaseFirstOne(className))
                         .addModifiers(Modifier.PUBLIC)
                         .addJavadoc("这里是注释" + route.group())
-                        .addParameter(String[].class, "args")
+//                        .addParameter(String[].class, "args")
                         .addCode("$T.getInstance()", ClassName.get("com.alibaba.android.arouter.launcher", "ARouter"))
                         .addCode(".build(\"" + route.path() + "\")")
                         .addCode(".navigation();")
@@ -128,8 +114,7 @@ public class ARouterProcessor extends BaseProcessor {
             if (CollectionUtils.isNotEmpty(annotations)) {
                 Set<? extends Element> routeElements = roundEnv.getElementsAnnotatedWith(Route.class);
                 try {
-
-                    //this.parseRoutes(routeElements);
+                    this.parseRoutes(routeElements);
                 } catch (Exception e) {
                     logger.error(e);
                 }
@@ -180,57 +165,10 @@ public class ARouterProcessor extends BaseProcessor {
 
             logger.info(">>> Found routes, size is " + routeElements.size() + " <<<");
 
-            rootMap.clear();
-
             TypeMirror type_Activity = elementUtils.getTypeElement(ACTIVITY).asType();
             TypeMirror type_Service = elementUtils.getTypeElement(SERVICE).asType();
             TypeMirror fragmentTm = elementUtils.getTypeElement(FRAGMENT).asType();
             TypeMirror fragmentTmV4 = elementUtils.getTypeElement(Consts.FRAGMENT_V4).asType();
-
-            // Interface of ARouter
-            TypeElement type_IRouteGroup = elementUtils.getTypeElement(IROUTE_GROUP);
-            TypeElement type_IProviderGroup = elementUtils.getTypeElement(IPROVIDER_GROUP);
-            ClassName routeMetaCn = ClassName.get(RouteMeta.class);
-            ClassName routeTypeCn = ClassName.get(RouteType.class);
-
-            /*
-               Build input type, format as :
-
-               ```Map<String, Class<? extends IRouteGroup>>```
-             */
-            ParameterizedTypeName inputMapTypeOfRoot = ParameterizedTypeName.get(
-                    ClassName.get(Map.class),
-                    ClassName.get(String.class),
-                    ParameterizedTypeName.get(
-                            ClassName.get(Class.class),
-                            WildcardTypeName.subtypeOf(ClassName.get(type_IRouteGroup))
-                    )
-            );
-
-            /*
-
-              ```Map<String, RouteMeta>```
-             */
-            ParameterizedTypeName inputMapTypeOfGroup = ParameterizedTypeName.get(
-                    ClassName.get(Map.class),
-                    ClassName.get(String.class),
-                    ClassName.get(RouteMeta.class)
-            );
-
-            /*
-              Build input param name.
-             */
-            ParameterSpec rootParamSpec = ParameterSpec.builder(inputMapTypeOfRoot, "routes").build();
-            ParameterSpec groupParamSpec = ParameterSpec.builder(inputMapTypeOfGroup, "atlas").build();
-            ParameterSpec providerParamSpec = ParameterSpec.builder(inputMapTypeOfGroup, "providers").build();  // Ps. its param type same as groupParamSpec!
-
-            /*
-              Build method : 'loadInto'
-             */
-            MethodSpec.Builder loadIntoMethodOfRootBuilder = MethodSpec.methodBuilder(METHOD_LOAD_INTO)
-                    .addAnnotation(Override.class)
-                    .addModifiers(PUBLIC)
-                    .addParameter(rootParamSpec);
 
             //  Follow a sequence, find out metas of group first, generate java file, then statistics them as root.
             for (Element element : routeElements) {
@@ -265,25 +203,13 @@ public class ARouterProcessor extends BaseProcessor {
                 } else {
                     throw new RuntimeException("The @Route is marked on unsupported class, look at [" + tm.toString() + "].");
                 }
-
                 categories(routeMeta);
             }
-
-            MethodSpec.Builder loadIntoMethodOfProviderBuilder = MethodSpec.methodBuilder(METHOD_LOAD_INTO)
-                    .addAnnotation(Override.class)
-                    .addModifiers(PUBLIC)
-                    .addParameter(providerParamSpec);
-
             Map<String, List<RouteDoc>> docSource = new HashMap<>();
 
             // Start generate java source, structure is divided into upper and lower levels, used for demand initialization.
             for (Map.Entry<String, Set<RouteMeta>> entry : groupMap.entrySet()) {
                 String groupName = entry.getKey();
-
-                MethodSpec.Builder loadIntoMethodOfGroupBuilder = MethodSpec.methodBuilder(METHOD_LOAD_INTO)
-                        .addAnnotation(Override.class)
-                        .addModifiers(PUBLIC)
-                        .addParameter(groupParamSpec);
 
                 List<RouteDoc> routeDocList = new ArrayList<>();
 
@@ -299,28 +225,6 @@ public class ARouterProcessor extends BaseProcessor {
                             List<? extends TypeMirror> interfaces = ((TypeElement) routeMeta.getRawType()).getInterfaces();
                             for (TypeMirror tm : interfaces) {
                                 routeDoc.addPrototype(tm.toString());
-
-                                if (types.isSameType(tm, iProvider)) {   // Its implements iProvider interface himself.
-                                    // This interface extend the IProvider, so it can be used for mark provider
-                                    loadIntoMethodOfProviderBuilder.addStatement(
-                                            "providers.put($S, $T.build($T." + routeMeta.getType() + ", $T.class, $S, $S, null, " + routeMeta.getPriority() + ", " + routeMeta.getExtra() + "))",
-                                            (routeMeta.getRawType()).toString(),
-                                            routeMetaCn,
-                                            routeTypeCn,
-                                            className,
-                                            routeMeta.getPath(),
-                                            routeMeta.getGroup());
-                                } else if (types.isSubtype(tm, iProvider)) {
-                                    // This interface extend the IProvider, so it can be used for mark provider
-                                    loadIntoMethodOfProviderBuilder.addStatement(
-                                            "providers.put($S, $T.build($T." + routeMeta.getType() + ", $T.class, $S, $S, null, " + routeMeta.getPriority() + ", " + routeMeta.getExtra() + "))",
-                                            tm.toString(),    // So stupid, will duplicate only save class name.
-                                            routeMetaCn,
-                                            routeTypeCn,
-                                            className,
-                                            routeMeta.getPath(),
-                                            routeMeta.getGroup());
-                                }
                             }
                             break;
                         default:
@@ -328,14 +232,12 @@ public class ARouterProcessor extends BaseProcessor {
                     }
 
                     // Make map body for paramsType
-                    StringBuilder mapBodyBuilder = new StringBuilder();
                     Map<String, Integer> paramsType = routeMeta.getParamsType();
                     Map<String, Autowired> injectConfigs = routeMeta.getInjectConfig();
                     if (MapUtils.isNotEmpty(paramsType)) {
                         List<RouteDoc.Param> paramList = new ArrayList<>();
 
                         for (Map.Entry<String, Integer> types : paramsType.entrySet()) {
-                            mapBodyBuilder.append("put(\"").append(types.getKey()).append("\", ").append(types.getValue()).append("); ");
 
                             RouteDoc.Param param = new RouteDoc.Param();
                             Autowired injectConfig = injectConfigs.get(types.getKey());
@@ -349,42 +251,11 @@ public class ARouterProcessor extends BaseProcessor {
 
                         routeDoc.setParams(paramList);
                     }
-                    String mapBody = mapBodyBuilder.toString();
-
-                    loadIntoMethodOfGroupBuilder.addStatement(
-                            "atlas.put($S, $T.build($T." + routeMeta.getType() + ", $T.class, $S, $S, " + (StringUtils.isEmpty(mapBody) ? null : ("new java.util.HashMap<String, Integer>(){{" + mapBodyBuilder.toString() + "}}")) + ", " + routeMeta.getPriority() + ", " + routeMeta.getExtra() + "))",
-                            routeMeta.getPath(),
-                            routeMetaCn,
-                            routeTypeCn,
-                            className,
-                            routeMeta.getPath().toLowerCase(),
-                            routeMeta.getGroup().toLowerCase());
 
                     routeDoc.setClassName(className.toString());
                     routeDocList.add(routeDoc);
                 }
-
-                // Generate groups
-                String groupFileName = NAME_OF_GROUP + groupName;
-                JavaFile.builder(PACKAGE_OF_GENERATE_FILE,
-                        TypeSpec.classBuilder(groupFileName)
-                                .addJavadoc(WARNING_TIPS)
-                                .addSuperinterface(ClassName.get(type_IRouteGroup))
-                                .addModifiers(PUBLIC)
-                                .addMethod(loadIntoMethodOfGroupBuilder.build())
-                                .build()
-                ).build().writeTo(mFiler);
-
-                logger.info(">>> Generated group: " + groupName + "<<<");
-                rootMap.put(groupName, groupFileName);
                 docSource.put(groupName, routeDocList);
-            }
-
-            if (MapUtils.isNotEmpty(rootMap)) {
-                // Generate root meta by group name, it must be generated before root, then I can find out the class of group.
-                for (Map.Entry<String, String> entry : rootMap.entrySet()) {
-                    loadIntoMethodOfRootBuilder.addStatement("routes.put($S, $T.class)", entry.getKey(), ClassName.get(PACKAGE_OF_GENERATE_FILE, entry.getValue()));
-                }
             }
 
             String json = JSON.toJSONString(docSource, SerializerFeature.PrettyFormat);
@@ -473,11 +344,9 @@ public class ARouterProcessor extends BaseProcessor {
      */
     private boolean routeVerify(RouteMeta meta) {
         String path = meta.getPath();
-
         if (StringUtils.isEmpty(path) || !path.startsWith("/")) {   // The path must be start with '/' and not empty!
             return false;
         }
-
         if (StringUtils.isEmpty(meta.getGroup())) { // Use default group(the first word in path)
             try {
                 String defaultGroup = path.substring(1, path.indexOf("/", 1));
