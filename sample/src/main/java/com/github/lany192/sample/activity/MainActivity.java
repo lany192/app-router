8993c77e56ddfd79138af5bf76c41869790e1426
Lany192 <lany192@qq.com>
2021-03-24 11:04:59 +0800

    update

--- sample/src/main/java/com/github/lany192/sample/activity/MainActivity.java
+++ sample/src/main/java/com/github/lany192/sample/activity/MainActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.alibaba.android.arouter.Router;
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.github.lany192.sample.Constants;
 import com.github.lany192.sample.R;
 import com.github.lany192.sample.User;
@@ -18,22 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.button1).setOnClickListener(v ->
-                ARouter.getInstance().build(Constants.APP_ONE)
-                        .withLong("ownerId", 666)
-                        .withBoolean("isFans", false)
-                        .navigation()
+                Router.get().oneActivity(66, true)
         );
         findViewById(R.id.button2).setOnClickListener(v ->
-                ARouter.getInstance().build(Constants.APP_TWO)
-                        .withLong("ownerId", 666)
-                        .withString("title", "这是一个标题")
-                        .navigation()
+                Router.get().twoActivity(88, "这是一个标题")
         );
         findViewById(R.id.button3).setOnClickListener(v ->
-                ARouter.getInstance().build(Constants.APP_THREE)
-                        .withString("username", "张三")
-                        .withObject("user", new User("李四", 888))
-                        .navigation()
+                Router.get().threeActivity("张三", new User("李四", 888))
         );
     }
 }
