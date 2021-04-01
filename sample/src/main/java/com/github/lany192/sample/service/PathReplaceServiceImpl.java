d727e8d9a0e255c0c60a2f5a10e820d991a04a22
Lany192 <lany192@qq.com>
2021-04-01 10:47:03 +0800

    update

--- sample/src/main/java/com/github/lany192/sample/service/PathReplaceServiceImpl.java
+++ sample/src/main/java/com/github/lany192/sample/service/PathReplaceServiceImpl.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
 
-import com.alibaba.android.arouter.RoutePath;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.facade.service.PathReplaceService;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * @author Administrator
@@ -14,27 +17,49 @@ import com.alibaba.android.arouter.facade.service.PathReplaceService;
 @Route(path = "/service/path/replace")
 public class PathReplaceServiceImpl implements PathReplaceService {
     private final String TAG = getClass().getSimpleName();
-    Context mContext;
+    /**
+     * 新旧路径映射表
+     */
+    private final Map<String, String> map = new HashMap<>();
 
     @Override
     public void init(Context context) {
-        mContext = context;
+        map.put("valuePack", "/app/send/coins/pay");
+        map.put("leaderboards", "/app/three");
     }
 
     @Override
     public String forString(String path) {
-        if ("/one".equals(path)) {
-            return RoutePath.APP_ONE;
-        }
-        return path;
+        return replace(path);
     }
 
     @Override
     public Uri forUri(Uri uri) {
-        Log.i(TAG, "Uri: " + uri);
-        if ("hello://leaderboards?age=18&username=张三".equals(uri.toString())) {
-            uri = Uri.parse("demo://m.test.com/app/three?age=18&username=张三");
+        return Uri.parse(replace(uri.toString()));
+    }
+
+    /**
+     * 路径转换
+     *
+     * @param path 路径
+     * @return 新路径
+     */
+    private String replace(String path) {
+        if (path.contains("//")) {
+            int beginIndex = path.indexOf("//");
+            String host;
+            if (path.contains("?")) {
+                int endIndex = path.indexOf("?");
+                host = path.substring(beginIndex + 2, endIndex);
+            } else {
+                host = path.substring(beginIndex + 2);
+            }
+            if (map.containsKey(host)) {
+                Log.i(TAG, "路径替换前path: " + path);
+                path = path.replace(host, Objects.requireNonNull(map.get(host)));
+                Log.i(TAG, "路径替换后path: " + path);
+            }
         }
-        return uri;
+        return path;
     }
 }
