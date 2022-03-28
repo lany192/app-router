ed15a9b34b17a4a7d5c9326f23c9d917ee43687b
lany192 <lany192@qq.com>
2022-03-28 22:54:00 +0800

    update

--- compiler/src/main/java/com/github/lany192/arouter/processor/AppRouterProcessor.java
+++ compiler/src/main/java/com/github/lany192/arouter/processor/AppRouterProcessor.java
@@ -71,7 +71,6 @@ public class AppRouterProcessor extends BaseProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(Route.class);
         if (elements != null && !elements.isEmpty()) {
-            createRoutePath(elements);
             List<MethodSpec> methods = new ArrayList<>();
             int index = 1;
             for (Element element : elements) {
@@ -100,44 +99,6 @@ public class AppRouterProcessor extends BaseProcessor {
             return false;
         }
         return true;
-    }
-
-    private void createRoutePath(Set<? extends Element> elements) {
-        List<FieldSpec> fields = new ArrayList<>();
-        for (Element element : elements) {
-            Route route = element.getAnnotation(Route.class);
-            String path = route.path();
-            String fieldName = path.replace("/", "_").toUpperCase();
-            //去掉第一个下划线
-            fieldName = fieldName.substring(1);
-            FieldSpec fieldSpec = FieldSpec
-                    .builder(String.class, fieldName, Modifier.STATIC, Modifier.PUBLIC, Modifier.FINAL)
-                    .addJavadoc(route.name() + "\n")
-                    .addJavadoc("类位置：{@link " + ClassName.get((TypeElement) element) + "}")
-                    .initializer("\"" + path + "\"")
-                    .build();
-            fields.add(fieldSpec);
-        }
-        TypeSpec.Builder builder = TypeSpec.classBuilder("RoutePath")
-                .addJavadoc("路径集合,自动生成,请勿编辑!")
-                .addModifiers(Modifier.PUBLIC);
-        builder.addFields(fields);
-        builder.addField(FieldSpec
-                .builder(String.class, "KEY_ROUTE_PATH", Modifier.STATIC, Modifier.PUBLIC, Modifier.FINAL)
-                .addJavadoc("登录界面需要携带的路径参数key(非界面路径)")
-                .initializer("\"route_path\"")
-                .build());
-        JavaFile javaFile = JavaFile
-                .builder("com.alibaba.android.arouter", builder.build())
-                // 设置表示缩进的字符串
-                .indent("    ")
-                .build();
-        try {
-            javaFile.writeTo(processingEnv.getFiler());
-        } catch (Exception e) {
-            logger.error(e);
-        }
-        logger.info("忽略异常提示");
     }
 
     /**
