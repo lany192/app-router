452680f1d3b6e11ccb5e16ee68d79726f57185f2
lany192 <lany192@qq.com>
2022-10-12 13:55:33 +0800

    update

--- green/src/main/java/com/github/lany192/green/HelloActivity.java
+++ green/src/main/java/com/github/lany192/green/HelloActivity.java
@@ -1,11 +1,10 @@
 package com.github.lany192.green;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.github.lany192.green.R;
 
 @Route(path = "/green/hello")
 public class HelloActivity extends AppCompatActivity {
@@ -13,6 +12,6 @@ public class HelloActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
+        setContentView(R.layout.activity_hello_green);
     }
 }
