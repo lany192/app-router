c3c925a88e035a80bbb7b7799587efb8b62717be
lany192 <lany192@qq.com>
2022-10-14 14:30:14 +0800

    update

--- blue/src/main/java/com/github/lany192/blue/HelloActivity.java
+++ blue/src/main/java/com/github/lany192/blue/HelloActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.github.lany192.common.GreenPaths;
 
 @Route(path = "/blue/hello")
 public class HelloActivity extends AppCompatActivity {
@@ -13,5 +15,6 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_blue);
+        findViewById(R.id.textView).setOnClickListener(v -> ARouter.getInstance().build(GreenPaths.GREEN_HELLO).navigation());
     }
 }
--- green/src/main/java/com/github/lany192/green/HelloActivity.java
+++ green/src/main/java/com/github/lany192/green/HelloActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.github.lany192.common.BluePaths;
 
 @Route(path = "/green/hello")
 public class HelloActivity extends AppCompatActivity {
@@ -13,5 +15,6 @@ public class HelloActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_green);
+        findViewById(R.id.textView).setOnClickListener(v -> ARouter.getInstance().build(BluePaths.BLUE_HELLO).navigation());
     }
 }
