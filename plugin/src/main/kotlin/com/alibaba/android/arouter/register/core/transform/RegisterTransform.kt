d26c4eec603213920b578d750b6b5221b01b6739
lany <lany192@qq.com>
2023-08-28 17:50:51 +0800

    update

--- plugin/src/main/kotlin/com/alibaba/android/arouter/register/core/transform/RegisterTransform.kt
+++ plugin/src/main/kotlin/com/alibaba/android/arouter/register/core/transform/RegisterTransform.kt
@@ -14,12 +14,14 @@ object RegisterTransform {
         val interface2 = "com.alibaba.android.arouter.facade.template.IRouteGroup"
         val interface3 = "com.alibaba.android.arouter.facade.template.IProviderGroup"
         val interface4 = "com.alibaba.android.arouter.facade.template.IProvider"
+        val interface5 = "com.alibaba.android.arouter.facade.template.IInterceptorGroup"
 
         val map = hashMapOf<String, ScanSetting>()
         map[interface1] = ScanSetting(interface1)
         map[interface2] = ScanSetting(interface2)
         map[interface3] = ScanSetting(interface3)
         map[interface4] = ScanSetting(interface4)
+        map[interface5] = ScanSetting(interface5)
         val androidComponents = project.extensions.getByType(AndroidComponentsExtension::class.java)
         androidComponents.onVariants { variant ->
             variant.instrumentation.transformClassesWith(
