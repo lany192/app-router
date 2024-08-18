324ae667e2922176ab3504a364d598865f3064f4
lany192 <lany192@qq.com>
2024-08-18 22:50:01 +0800

    refactor(compiler): 实现Autowired注解的KSP处理器
    添加了对Autowired注解的支持，该注解适用于ARouter库。此实现使用Kotlin符号处理(KSP)生成适当的注入代码，
    显著减少手动注入字段的需求。该特性通过新的KSP处理器实现，该处理器在编译时检查和处理具有@Autowired注解的字段。
    
    BREAKING CHANGE: 该重构可能影响现有的非KSP编译流程。请确保在升级前测试新的KSP编译环境。

---
+++ compiler/src/main/java/com/alibaba/android/arouter/ksp/compiler/entity/RouteMetaKspExt.kt
@@ -0,0 +1,7 @@
+package com.alibaba.android.arouter.ksp.compiler.entity
+
+import com.alibaba.android.arouter.facade.model.RouteMeta
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+
+internal val RouteMeta.kspRawType: KSClassDeclaration
+    get() = (this as RouteMetaKsp).kspRawTypeInner
